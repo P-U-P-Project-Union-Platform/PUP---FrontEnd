fd25c43c8cd0533edca7c7b5dfaa9115bac2ae84
Gri22ly <79156923+Junseung0526@users.noreply.github.com>
2026-02-04 11:07:42 +0900

    feat: 통계 표시용 StatCard 컴포넌트 생성

---
+++ src/components/admin/common/StatCard.tsx
@@ -0,0 +1,35 @@
+import {
+  StatCardContainer,
+  StatHeader,
+  StatTitle,
+  StatIcon,
+  StatValue,
+  StatChange
+} from '../../../styles/components/admin/statCardStyles';
+
+interface StatCardProps {
+  title: string;
+  value: number | string;
+  change?: string;
+  icon: string;
+  gradient: string;
+}
+
+export default function StatCard({ title, value, change, icon, gradient }: StatCardProps) {
+  const isPositive = change ? !change.startsWith('-') : true;
+
+  return (
+    <StatCardContainer gradient={gradient}>
+      <StatHeader>
+        <StatTitle>{title}</StatTitle>
+        <StatIcon>{icon}</StatIcon>
+      </StatHeader>
+      <StatValue>{value}</StatValue>
+      {change && (
+        <StatChange isPositive={isPositive}>
+          {change}
+        </StatChange>
+      )}
+    </StatCardContainer>
+  );
+}
